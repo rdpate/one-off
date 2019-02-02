9dc1133e4f6f3c7498a437e9fbab99b2c14ce020
Roger Pate <rogerpate@gmail.com>
2019-02-02 10:05:36 -0500

    [squote.sh] loop and make snippet executable

--- squote.sh
+++ squote.sh
@@ -1,7 +1,15 @@
+#!/bin/sh -Cue
+
 squote() {
-    if [ -n "$1" ] && [ x"$(printf %s "$1" | tr -d -c \\055_/0-9A-Za-z)" = x"$1" ]; then
-        printf %s "$1"
-    else
-        printf \'%s\' "$(printf %s "$1" | sed -r s/\'/\'\\\\\'\'/g)"
-        fi
+    while [ $# != 0 ]; do
+        if [ -n "$1" ] && [ x"$(printf %s "$1" | tr -d -c \\055_/0-9A-Za-z)" = x"$1" ]; then
+            printf %s${2+' '} "$1"
+        else
+            printf \'%s\'${2+' '} "$(printf %s "$1" | sed -r s/\'/\'\\\\\'\'/g)"
+            fi
+        shift
+        done
     }
+
+squote "$@"
+printf \\n
